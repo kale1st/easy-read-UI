f72880a9928132b811784e0ea244938f59987d1e
azizkale <azizkale@hotmail.com>
2024-02-03 13:39:21 +0100

    form builder removed from kitaplik

--- src/app/Display/displaypir/chapters/chapters.component.ts
+++ src/app/Display/displaypir/chapters/chapters.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DisplaypirService } from 'src/app/services/displaypir.service';
 import { Pir } from 'src/models/Pir';
@@ -10,17 +9,14 @@ import { Pir } from 'src/models/Pir';
   styleUrls: ['./chapters.component.css']
 })
 export class ChaptersComponent implements OnInit {
-  retrieveChaptersNamesForm!: FormGroup
   selectedPirId: any;
   pir!: Pir
   chaptersNames!: any[]
 
   constructor(
-    public fb: FormBuilder,
     private activeroute: ActivatedRoute,
     private displaypirservice: DisplaypirService
   ) {
-    this.formChaptersNames();
   }
 
   async ngOnInit() {
@@ -29,22 +25,11 @@ export class ChaptersComponent implements OnInit {
     await this.retrieveChaptersNamesByPirId();
   }
 
-  formChaptersNames() {
-    this.retrieveChaptersNamesForm = this.fb.group({
-      chapterName: this.fb.array([])
-    });
-    //formname array is fullfilled in the retrievePirs function (below)
-  }
-
-
 
   async retrieveChaptersNamesByPirId() {
     await this.displaypirservice.retrieveChaptersNamesByPirId(this.selectedPirId).subscribe({
       next: async (data) => {
         this.chaptersNames = data
-        this.chaptersNames.forEach((chapter) => {
-          this.retrieveChaptersNamesForm.addControl(chapter.chapterName, new FormControl(chapter.chapterName));
-        });
       }
     })
   }
