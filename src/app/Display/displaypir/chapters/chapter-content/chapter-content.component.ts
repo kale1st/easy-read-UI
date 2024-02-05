c4b06a864ce5903a21483dd8ea2e114acc18ea9d
azizkale <azizkale@hotmail.com>
2024-02-05 11:45:34 +0100

    unnecessary form attribute removed

--- src/app/Display/displaypir/chapters/chapter-content/chapter-content.component.ts
+++ src/app/Display/displaypir/chapters/chapter-content/chapter-content.component.ts
@@ -1,5 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DisplaypirService } from 'src/app/services/displaypir.service';
 import { Chapter } from 'src/models/Chapter';
@@ -13,98 +18,99 @@ import { Pir } from 'src/models/Pir';
   selector: 'app-chapter-content',
   templateUrl: './chapter-content.component.html',
   styleUrls: ['./chapter-content.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ChapterContentComponent implements OnInit {
-  @ViewChild('chapterContent') chapterContent!: ElementRef
+  @ViewChild('chapterContent') chapterContent!: ElementRef;
 
-  selectedChapterId: any
+  selectedChapterId: any;
   selectedPirId: any;
-  selectedChapter!: Chapter
+  selectedChapter!: Chapter;
   selectedPirName!: String;
-  retrieveChapterContentForm!: FormGroup
 
   fontSize!: number;
   lineHeight!: number;
   isNightMode!: boolean;
 
   constructor(
-    private fb: FormBuilder,
     private activeroute: ActivatedRoute,
     private displaypirservice: DisplaypirService,
     private dialog: MatDialog,
     private pirService: PireditService
-  ) {
-    this.formChapterRetrieve()
-
-  }
+  ) {}
 
   async ngOnInit() {
     this.initialReadMode();
     this.initialFontSetting();
-    this.selectedChapterId = await this.activeroute.snapshot.paramMap.get('contentId');
+    this.selectedChapterId = await this.activeroute.snapshot.paramMap.get(
+      'contentId'
+    );
     this.selectedPirId = await this.activeroute.snapshot.paramMap.get('pirId');
-    this.retrieveChaptertByChapterId()
+    this.retrieveChaptertByChapterId();
   }
-
-  formChapterRetrieve() {
-    this.retrieveChapterContentForm = this.fb.group({
-      chapterId: ['', Validators.required],
-      pirId: ['', Validators.required],
-      editorId: ['', Validators.required],
-      createDate: ['', Validators.required],
-      chapterContent: ['', Validators.required],
-    });
-
-  }
-
 
   retrieveChaptertByChapterId() {
-    this.displaypirservice.retrieveChapterByChapterId(this.selectedChapterId, this.selectedPirId).subscribe({
-      next: async (chapter: Chapter) => {
-        this.selectedChapter = await chapter;
+    this.displaypirservice
+      .retrieveChapterByChapterId(this.selectedChapterId, this.selectedPirId)
+      .subscribe({
+        next: async (chapter: Chapter) => {
+          this.selectedChapter = await chapter;
+          if (
+            this.selectedChapter.wordPairs !== undefined &&
+            this.selectedChapter.wordPairs.length > 0
+          ) {
+            //modifies the chapter content adding <b> tag to wordpairs
+            for (const wordpair of Object.values(
+              this.selectedChapter.wordPairs
+            )) {
+              this.selectedChapter.chapterContent =
+                this.selectedChapter.chapterContent.replace(
+                  wordpair.word.trim(),
+                  `<b>${wordpair.word.trim()}</b>`
+                );
+              this.chapterContent.nativeElement.innerHTML =
+                this.selectedChapter.chapterContent;
+            }
 
-        if (this.selectedChapter.wordPairs !== undefined) {
-          //modifies the chapter content adding <b> tag to wordpairs
-          for (const wordpair of Object.values(this.selectedChapter.wordPairs)) {
-            this.selectedChapter.chapterContent = this.selectedChapter.chapterContent.replace(wordpair.word.trim(), `<b>${wordpair.word.trim()}</b>`);
-            this.chapterContent.nativeElement.innerHTML = this.selectedChapter.chapterContent
-          }
-
-          // adding mouseover event to the <b> tags
-          Object.values(this.chapterContent.nativeElement.getElementsByTagName('b')).map((el: HTMLElement | any) => {
-            el.addEventListener('click', async () => {
-              // getting meaning from wordPairs
-              const word_: WordPair | any = Object.values(this.selectedChapter.wordPairs).find((pair: WordPair) => pair.word.trim() === el.innerHTML.trim())
-              //popup
-              this.openDialog(word_)
+            // adding mouseover event to the <b> tags
+            Object.values(
+              this.chapterContent.nativeElement.getElementsByTagName('b')
+            ).map((el: HTMLElement | any) => {
+              el.addEventListener('click', async () => {
+                // getting meaning from wordPairs
+                const word_: WordPair | any = Object.values(
+                  this.selectedChapter.wordPairs
+                ).find(
+                  (pair: WordPair) => pair.word.trim() === el.innerHTML.trim()
+                );
+                //popup
+                this.openDialog(word_);
+              });
             });
-          })
-        }
-        else {
-          this.chapterContent.nativeElement.innerHTML = chapter.chapterContent
-        }
-        this.getPirNameByPirId();
-      }
-    })
+          } else {
+            this.chapterContent.nativeElement.innerHTML =
+              chapter.chapterContent;
+          }
+          this.getPirNameByPirId();
+        },
+      });
   }
 
   getPirNameByPirId() {
     this.pirService.retrievePirByPirId(this.selectedPirId).subscribe({
       next: (pir: Pir) => {
-        this.selectedPirName = pir.name
-      }
+        this.selectedPirName = pir.name;
+      },
     });
   }
 
   openDialog(wordpair: WordPair): void {
     if (wordpair !== undefined) {
       this.dialog.open(DialogComponent, {
-        data: { word: wordpair.word, meaning: wordpair.meaning }
+        data: { word: wordpair.word, meaning: wordpair.meaning },
       });
     }
   }
-
 
   increaseFontSize() {
     this.fontSize += 1.3;
@@ -144,7 +150,6 @@ export class ChapterContentComponent implements OnInit {
 
   readModeClass(): string {
     return this.isNightMode ? 'night-mode' : 'light-mode';
-
   }
 
   initialReadMode() {
@@ -157,7 +162,10 @@ export class ChapterContentComponent implements OnInit {
 
   changeReadMode() {
     this.isNightMode = !this.isNightMode;
-    localStorage.setItem('readMode', this.isNightMode ? 'night-mode' : 'light-mode');
+    localStorage.setItem(
+      'readMode',
+      this.isNightMode ? 'night-mode' : 'light-mode'
+    );
     this.readModeClass();
   }
 }
